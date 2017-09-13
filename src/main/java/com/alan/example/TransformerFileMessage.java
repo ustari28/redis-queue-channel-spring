756bcdb00ec9463a5f04f4d753f5d148b48789d1
ALAN GABRIEL DAVILA RISCO <alan1754@hotmail.com>
2017-09-13 23:18:57 +0200

    We use mark for processing file when load finishes and we send this mark messages to nullChannel

--- src/main/java/com/alan/example/TransformerFileMessage.java
+++ src/main/java/com/alan/example/TransformerFileMessage.java
@@ -3,6 +3,12 @@ package com.alan.example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.file.splitter.FileSplitter;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Alan Dávila<br>
@@ -12,10 +18,26 @@ import org.springframework.integration.annotation.Transformer;
 @MessageEndpoint
 public class TransformerFileMessage {
 
+
     @Transformer
-    public String transformerFileMessage(final String input) throws InterruptedException {
+    public String transformerFileMessage(final String input, MessageHeaders headers) throws InterruptedException {
         log.info("mensaje entrante");
-        Thread.sleep(100);
+        //Thread.sleep(100);
         return input;
+    }
+
+    @Transformer
+    public String transformerFileMessageMark(final FileSplitter.FileMarker fileMarker) {
+        FileSplitter.FileMarker.Mark mark = fileMarker.getMark();
+        Map<String, Object> mapHeaders = new HashMap<>();
+        MessageHeaders newHeaders = new MessageHeaders(mapHeaders);
+        log.info("File {} mark {} with {} line(s)", fileMarker.getFilePath(), mark.name(), fileMarker.getLineCount());
+        return null;
+    }
+
+    @Transformer(inputChannel = "channel-recieves-files")
+    public String zipFile(final GenericMessage<String> message) {
+
+        return "File has been finished " + message.getHeaders().get("file_name");
     }
 }
